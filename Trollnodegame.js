3e54a00c136a3146cc3c9125fe41b4477a08c7e7
Alex Gehring <alex.science5@gmail.com>
2017-02-25 09:34:59 -0600

    Add second rl.question

--- Trollnodegame.js
+++ Trollnodegame.js
@@ -6,12 +6,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-function fightPayRun(input){
-  switch (input){
-    case 'fight':
-    colorPrompt("You're fighting the troll.", "red")
-  }
-}
+
 function colorPrompt(prompt, color = "blue"){
   return console.log(chalk[color](prompt))
 }
@@ -20,12 +15,26 @@ const getPrompt = () => {
     console.log(chalk.cyan('Bye!'));
     return process.exit();
   });
+
   colorPrompt("You're walking through the forest, minding your own business.", "green")
   rl.question("...and you run into a troll!\n Do you FIGHT him, PAY him, or RUN?", (userInput) => {
       let input = userInput.toLowerCase().trim()
       fightPayRun(input)
-      getPrompt();
+
   });
 }
+
+function fightPayRun(input){
+  switch (input){
+    case 'fight':
+    colorPrompt("You're fighting the troll.", "red");
+    var strong = rl.question("Are you strong? Yes or No", (userInput) => {
+      let input = userInput.toLowerCase().trim()
+      fightPayRun(input)
+    });
+
+  }
+}
+
 
 getPrompt();
