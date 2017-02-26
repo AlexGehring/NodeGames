65d26223fb44bdfd6ebbfc7b51d3792680ac5c34
Alex Gehring <alex.science5@gmail.com>
2017-02-26 17:45:37 -0600

    Add pay case

--- Trollnodegame.js
+++ Trollnodegame.js
@@ -41,11 +41,36 @@ function fightPayRun(input){
         })
       }else{
         rl.question("That's too bad, but are you smart?", (userInput) => {
-          let smart = userInput.lowerCase().trim()
+          let smart = userInput.toLowerCase().trim()
           if(smart === 'yes'){
             colorPrompt("You did it! You out smarted the beast!", "yellow")
           } else {
             colorPrompt("No smarts and no strength? You lose!", "Red")
+          }
+        })
+      } 
+    })
+    break;
+    case 'pay':
+    colorPrompt("You've just paid the troll...", "red")
+    rl.question("Safe choice, but do you have any money? Yes or No", (userInput) => {
+      let money = userInput.toLowerCase().trim()
+      if (money === 'yes'){
+        rl.question("That's great! But do you have Troll Bills? Yes or No", (userInput) => {
+          let trollBills = userInput.toLowerCase().trim()
+          if (trollBills === 'yes'){
+            colorPrompt("You've defeated the Troll! Nice Job!", "yellow")
+          }else{
+            colorPrompt("You only needed one of the two to win! You did it!", "yellow")
+          }
+        })
+      }else{
+        rl.question("That's too bad, do you have any jewels?", (userInput) => {
+          let jewels = userInput.toLowerCase().trim()
+          if(jewels === 'yes'){
+            colorPrompt("You did it! Trolls are greedy and will take your jewels as trade!", "yellow")
+          } else {
+            colorPrompt("No money or jewels!? You lose!", "Red")
           }
         })
       }
