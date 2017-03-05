962f39a12368fd5be5328af66d7da91583541d43
Alex Gehring <alex.science5@gmail.com>
2017-03-04 20:49:11 -0600

    add options to game

--- badLibsNode.js
+++ badLibsNode.js
@@ -30,8 +30,48 @@ const getPrompt = () => {
 function playMyGame(input){
   switch (input){
     case 'yes':
-        rl.question("Type a noun of your choice:", (userInput) => {
-          let noun = userInput.toLowerCase().trim()
+        rl.question("Type an adverb of your choice:", (userInput) => {
+          let adverb = userInput.toLowerCase().trim()
+          if (adverb){
+            colorPrompt ("You chose the adverb: " + adverb, "red");
+            rl.question("Type a noun of your choice:", (userInput) => {
+              let noun = userInput.toLowerCase().trim()
+              if (noun){
+                colorPrompt("You chose the noun: " + noun, "red");
+                rl.question("Type a verb in the present tense:", (userInput) => {
+                  let verbPresent = userInput.toLowerCase().trim()
+                  if (verbPresent){
+                    colorPrompt("You chose the verb: " + verbPresent, "red");
+                    rl.question("Type a plural noun:", (userInput) => {
+                      let pluralNoun = userInput.toLowerCase().trim()
+                      if (pluralNoun){
+                        colorPrompt("you chose the noun: " + pluralNoun, "red");
+                        rl.question("Type a verb in the past tense:", (userInput) => {
+                          let verbPast = userInput.toLowerCase().trim()
+                          if (verbPast){
+                            colorPrompt("You chose the verb: " + verbPast, "red");
+
+                          } else {
+                            colorPrompt ("You didnt enter a past tense verb.")
+                          }
+                        })
+                      } else {
+                        colorPrompt ("you didnt enter a noun.")
+                      }
+                    });
+
+
+                  }else {
+                    colorPrompt("you didnt enter a verb")
+                  }
+                })
+              } else {
+                colorPrompt("you didnt enter a noun", "red");
+              }
+            })
+          }else {
+            colorPrompt("you didnt enter an adverb", "red")
+          }
         });
 
     default:
